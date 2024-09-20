e739ab4196b8d7028c351fd06665b99835b07114
coucoubobby <coucoubobbyetatmosprimebobby@gmail.com>
2024-09-20 03:26:01 +0000

    Update FauxData.sh

--- FauxData.sh
+++ FauxData.sh
@@ -15,10 +15,10 @@ echo -e "\033[1;32m==============================\033[0m"
 
 generate_fake_info() {
     local username=$1
-    local ip_address="$((RANDOM % 255)).$((RANDOM % 255)).>
-    local phone_number="+1$((RANDOM % 900 + 100))$((RANDOM>
+    local ip_address="$((RANDOM % 255)).$((RANDOM % 255)).$((RANDOM % 255)).$((RANDOM % 255))"
+    local phone_number="+1$((RANDOM % 900 + 100))$((RANDOM % 100 + 200))"
     local id_card="$((RANDOM % 900000000 + 100000000))"
-    local credit_card="$((RANDOM % 9000000000000000 + 1000>
+    local credit_card="$((RANDOM % 9000000000000000 + 100000000000))"
     local wifi_pass="$((RANDOM % 100000000))"
     echo "Username: $username"
 
@@ -30,7 +30,7 @@ generate_fake_info() {
 }
 
 # Main script
-echo "Welcome To FauxData - Get Peoples Personal Info, ple>
+echo "Welcome To FauxData - Get Peoples Personal Info, please read the README.md of https://github.com/coucoubobby/FauxData before using."
 read -p "Write username of a person: " username
 
     echo "Username found, stealing info..."
